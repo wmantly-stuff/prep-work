54bb07c4bc504a975e71481f61fc13fb5b2a9c30
Armen Vartan <vartan.armen@gmail.com>
2014-12-01 17:23:07 -0500

    deleted js files

--- python/4-while-loops-and-for-each/main.js
+++
@@ -1,8 +0,0 @@
-// set your variables below
-
-// all jquery dom actions can only be called inside document ready. Print your variables
-// inside document ready.
-
-$(document).ready(function(){
-
-})
