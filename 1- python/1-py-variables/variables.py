aa212126a34e2afa1167f0a770759d365000a3a1
Amos Jones <azalexkamara@gmail.com>
2015-08-27 18:29:47 -0400

    completed variables.py

---
+++ 1- python/1-py-variables/variables.py
@@ -0,0 +1,5 @@
+byte = "School"
+instructors = ["Greg", "Ken", "Billy"]
+students = ("Adolfo", "Benny", "Devin", "Brendan")
+d = {"byteAcademy":byte, "instructors":instructors, "students":students}
+print(d)
